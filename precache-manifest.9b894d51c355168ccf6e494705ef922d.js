1e2d2c4670cddacc2fbda866060d9c27345b5cf6
Tal Adari <tal0392@gmail.com>
2019-03-31 16:24:00 +0300

    Updates

---
+++ precache-manifest.9b894d51c355168ccf6e494705ef922d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9be4b39fc5711f8d08677beecfb1bf3",
+    "url": "/llidesign-mock/static/media/3.c9be4b39.jpg"
+  },
+  {
+    "revision": "ba4e20caff7f9f1940bb",
+    "url": "/llidesign-mock/static/css/main.e0a0f7b7.chunk.css"
+  },
+  {
+    "revision": "0fff954fb196735474e2",
+    "url": "/llidesign-mock/static/js/runtime~main.74bcb7c9.js"
+  },
+  {
+    "revision": "ded73dc3830fb2d80c34",
+    "url": "/llidesign-mock/static/js/2.b7dd99f8.chunk.js"
+  },
+  {
+    "revision": "4439a9ad37fe20d5bae4979d851c8539",
+    "url": "/llidesign-mock/static/media/1.4439a9ad.jpg"
+  },
+  {
+    "revision": "cd2a32c74ec4ba2969dc696b9fe4f4c4",
+    "url": "/llidesign-mock/static/media/2.cd2a32c7.jpg"
+  },
+  {
+    "revision": "ba4e20caff7f9f1940bb",
+    "url": "/llidesign-mock/static/js/main.5484fbb0.chunk.js"
+  },
+  {
+    "revision": "f3b7a84741fdc74ccfef1d955207c4ab",
+    "url": "/llidesign-mock/static/media/4.f3b7a847.jpg"
+  },
+  {
+    "revision": "f0eef03ad24aefc9e011e0146729e7ac",
+    "url": "/llidesign-mock/static/media/5.f0eef03a.jpg"
+  },
+  {
+    "revision": "50a3d5f9535bccd44dbf35a1a8921159",
+    "url": "/llidesign-mock/static/media/6.50a3d5f9.jpg"
+  },
+  {
+    "revision": "67b99ea1520e944b9b2a94de38f6f7af",
+    "url": "/llidesign-mock/static/media/7.67b99ea1.jpg"
+  },
+  {
+    "revision": "6e8ce7e8f8d95673adf20b4c8f5fddf9",
+    "url": "/llidesign-mock/index.html"
+  }
+];
